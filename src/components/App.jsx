6d61f7fc1afb42a745ec27047fafdd93e2e4eb39
Katrinnk <katerin69llobster@gmail.com>
2023-09-21 15:58:06 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import { Component } from 'react';
 import { LeaveFeedback } from './leaveFeedback';
+import { Notification } from './Notification';
 import { Statistics } from './satisticsFeedback';
+import { Section } from './Section';
 
 class App extends Component {
   state = { good: 0, neutral: 0, bad: 0 };
@@ -10,17 +12,40 @@ class App extends Component {
       return { [key]: prev[key] + 1 };
     });
   };
+  getTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  getPositiveFeedback = () => {
+    const { good } = this.state;
+    const total = this.getTotalFeedback();
+    return good / total;
+  };
 
   render() {
+    const totalFeedback = this.getTotalFeedback();
+    const positiveFeedback = this.getPositiveFeedback();
     return (
       <>
-        <LeaveFeedback leaveFeedback={this.onLeaveFeedback} />
-        {this.state.good === 0 &&
-        this.state.neutral === 0 &&
-        this.state.bad === 0 ? (
-          <h2 className="mt-3 mb-2 ms-3">There is no feedback</h2>
+        <Section title="Please leave feedback">
+          <LeaveFeedback
+            leaveFeedback={this.onLeaveFeedback}
+            options={Object.keys(this.state)}
+          />
+        </Section>
+        {!totalFeedback ? (
+          <Notification message="There is no feedback" />
         ) : (
-          <Statistics feedback={this.state} />
+          <Section title="Statistics">
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={totalFeedback}
+              positivePercentage={positiveFeedback}
+            />
+          </Section>
         )}
       </>
     );
--- src/components/satisticsFeedback/index.jsx
+++ src/components/satisticsFeedback/index.jsx
@@ -1,28 +1,27 @@
-import { v4 as uuidv4 } from 'uuid';
-
-export const Statistics = ({ feedback }) => {
-  const { good, neutral, bad } = feedback;
-  const totalFeedback = () => {
-    return good + neutral + bad;
-  };
-
-  const positiveFeedback = () => {
-    const total = totalFeedback();
-    return good / total;
-  };
-
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => {
   return (
     <>
-      <h2 className="mt-3 mb-2 ms-3">Statistics</h2>
       <ul className="list-group ms-3 me-3">
-        {Object.entries(feedback).map(([key, value]) => (
-          <li key={uuidv4()} className="list-group-item">
-            {key}: {value}
-          </li>
-        ))}
-        <li className="list-group-item">Total: {totalFeedback()}</li>
-        <li className="list-group-item">
-          Positive Feedback: {positiveFeedback()}%
+        <li className="list-group-item" key="good">
+          Good: {good}
+        </li>
+        <li className="list-group-item" key="neutral">
+          Neutral: {neutral}
+        </li>
+        <li className="list-group-item" key="bad">
+          Bad: {bad}
+        </li>
+        <li className="list-group-item" key="total">
+          Total: {total}
+        </li>
+        <li className="list-group-item" key="positivePercentage">
+          Positive Feedback: {positivePercentage}%
         </li>
       </ul>
     </>
